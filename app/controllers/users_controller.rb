473d4cf0deb3f3e41bf3333e0f1e36ef2997194f
Stephen Crane <scrane88@gmail.com>
2021-10-18 21:56:03 -0400

    add authorization for user profiles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,20 @@
-class UserController < ApplicationController
+class UsersController < ApplicationController
+  before_action :authenticate_user!
+
   def show
+    @user = User.find_by(id: params[:id])
+    authorize
+  end
+
+private
+
+  def authorize
+    if !@user
+      flash[:warning] = "Could not find that user"
+      redirect_to :root
+    elsif @user != current_user
+      flash[:warning] = "You do not have permission to view that profile"
+      redirect_to :root, code: :forbidden
+    end
   end
 end
