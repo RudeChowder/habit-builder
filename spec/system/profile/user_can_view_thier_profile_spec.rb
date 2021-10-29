7aa72e834581104633061c0ffd5ebe3804f33200
Stephen Crane <scrane88@gmail.com>
2021-10-29 08:09:35 -0400

    first feature test

---
+++ spec/system/profile/user_can_view_thier_profile_spec.rb
@@ -0,0 +1,25 @@
+require "rails_helper"
+
+describe "When a user views their profile", type: :system do
+  before do
+    @user = create(:user)
+    sign_in @user
+  end
+
+  it "they can see their info" do
+    habit = create(:habit, name: "Write system tests")
+    goal = create(:goal, user: @user, habit: habit, target: 100)
+    checkin = build(:checkin, user: @user, date: Date.today)
+    checkin.habits << habit
+    checkin.save
+
+    visit "/"
+
+    click_link "Profile"
+
+    expect(page).to have_content "Write system tests"
+    expect(page).to have_content "Target: 100 days"
+    expect(page).to have_content "#{checkin.pretty_date}"
+
+  end
+end
